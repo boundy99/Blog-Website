ea7c8829789951a2086097059c403a494c819c0a
boundy99 <abdoulayeboundydjikine@gmail.com>
2022-11-18 21:07:43 -0500

    4

--- app.js
+++ app.js
@@ -12,22 +12,13 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/', (req, res) => {
+    res.render('home', { paragraph: homeStartingContent })
+})
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+    console.log("Server started on port 3000");
 });
