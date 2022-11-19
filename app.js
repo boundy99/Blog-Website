8288a1e1459228a4b5981d1f18304970bbcdcbd7
boundy99 <abdoulayeboundydjikine@gmail.com>
2022-11-18 21:27:19 -0500

    About Contact Home pages

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ app.get('/', (req, res) => {
     res.render('home', { paragraph: homeStartingContent })
 })
 
+app.get('/about', (req, res) => {
+    res.render('about', { paragraph: aboutContent })
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact', { paragraph: contactContent })
+})
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
